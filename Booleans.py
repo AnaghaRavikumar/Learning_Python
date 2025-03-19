8cf9acd9767d31209a3e09a89186250e8e1089a9
AnaghaRavikumar <aghirm@gmail.com>
2025-03-19 22:49:15 +0530

    Booleans in python3

---
+++ Booleans.py
@@ -0,0 +1,22 @@
+#Boolean Values: True, Flase
+
+#Booleans are used when comparing objects
+a = 3
+b = 5
+
+print(a == b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(type(True))
+ #All numbers positive or negative are converted to True 0 is false
+print(bool(-1.75))
+print(bool(0))
+
+#All strings are converted to True, empty string is false
+
+print(bool("Turings"))
+print(bool(""))
+print(str(True))
+print(5+True)
+print(6*False)
