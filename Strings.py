22b87cc1e6b86f3d07d3cb2e157418e99aa2d33c
AnaghaRavikumar <aghirm@gmail.com>
2025-03-19 22:01:20 +0530

    Strings Commit - Socratica

---
+++ Strings.py
@@ -0,0 +1,29 @@
+message = "Meet me tonight."
+print(message)
+
+message2 = 'The clock strikes at midnight.'
+print(message2)
+
+'''
+syntax Error  - the apostrophe made python think it is ending there and rest of string caused error
+message3 = 'I'm looking for someone to share in an adventure'
+'''
+message3 = 'I\'m looking for someone to share in an adventure'
+print(message3)
+'''
+Its always prefered to use ""
+'''
+
+'''
+Invalid Syntax
+message4 = "The phrase "Beam me up, Scotty" was never said on Star Trek."
+'''
+
+message4 = 'The phrase "Beam me up, Scotty" was never said on Star Trek.'
+
+movie_quote = """ One of my favourite lines from The Godfather  is:
+"I'm going to make him an offer he can't refuse."
+Do you know who said this?"""
+
+print(movie_quote)
+
